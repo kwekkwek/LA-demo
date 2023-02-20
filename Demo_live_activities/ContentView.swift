39d9f5bc36d7c16396aeac49f4e02532caa89326
Benny <benny.kurniawan@tokopedia.com>
2023-02-20 16:04:08 +0700

    Initial Commit

---
+++ Demo_live_activities/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Demo_live_activities
+//
+//  Created by benny.kurniawan on 20/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo_live_activities/Demo_live_activitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_live_activitiesApp.swift
+//  Demo_live_activities
+//
+//  Created by benny.kurniawan on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_live_activitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
